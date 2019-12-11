17875e7c43ac234816d4b20b2ad6ac9374ea3143
louiskuhn <kuhnlouis@gmail.com>
2019-12-11 11:33:47 +0100

    ajout regressions

---
+++ Ressources/Régressions/Régressions linéaires/Régression linéaire simple/implementation_regression_lineaire_simple.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib qt5
+
+# Données au choix (tester pour les 2)
+# les données salaires.csv à importer
+
+#possibilité de générer des données aléatoirement avec sklearn
+#from sklearn.datasets import make_regression
+#x, y= make_regression(n_samples = 100, n_features = 1, noise = 10)
+#y = y.reshape(y.shape[0],1)
